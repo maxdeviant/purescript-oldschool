ad3e03d3f8d13c4b2e98f418a7907090457bfaef
Marshall Bowers <elliott.codes@gmail.com>
2021-11-14 17:55:04 -0500

    Spago init

---
+++ shell.nix
@@ -0,0 +1,22 @@
+with import <nixpkgs> {};
+
+let
+  easy-ps = import (fetchFromGitHub {
+    owner = "justinwoo";
+    repo = "easy-purescript-nix";
+    rev = "7802db65618c2ead3a55121355816b4c41d276d9";
+    sha256 = "0n99hxxcp9yc8yvx7bx4ac6askinfark7dnps3hzz5v9skrvq15q";
+  }) {
+    inherit pkgs;
+  };
+in
+stdenv.mkDerivation {
+  name = "oldschool";
+
+  buildInputs = [
+    easy-ps.purs
+    easy-ps.purs-tidy
+    easy-ps.spago
+    easy-ps.pulp
+  ];
+}
---
+++ spago.dhall
@@ -0,0 +1,5 @@
+{ name = "oldschool"
+, dependencies = [ "console", "effect", "prelude", "psci-support" ]
+, packages = ./packages.dhall
+, sources = [ "src/**/*.purs", "test/**/*.purs" ]
+}
